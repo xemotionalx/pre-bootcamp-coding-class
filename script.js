f8214e92c9cfa3aa3cf321fa0be0b4db2ec9ef14
MaiAda Carpano <52050339+xemotionalx@users.noreply.github.com>
2021-06-06 14:12:37 -0400

    added logic to reverse flip cards to all cards

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-function reverseCard(selected) {
-    var selectedCard = document.getElementById("card-" + selected);
-    var imageId = document.getElementById("card__img-" + selected);
-    var textId = document.getElementById("card__text-" + selected);
-    var listId = document.getElementById("card__links-" + selected);
+var htmlCardIsFlipped = false;
+var cssCardIsFlipped = false;
+var javascriptCardIsFlipped = false;
+var designCardIsFlipped = false;
 
-    imageId.style.display = "none";
-    textId.style.display = "none";
-    listId.style.display= "block";
+function reverseCard(selectedCard) {
+    var imageId = document.getElementById("card__img-" + selectedCard);
+    var textId = document.getElementById("card__text-" + selectedCard);
+    var listId = document.getElementById("card__links-" + selectedCard);
     
+    var selectedCardIsFlipped;
+
+    if (selectedCard === "html") {
+        selectedCardIsFlipped = htmlCardIsFlipped;
+    } else if (selectedCard === "css") {
+        selectedCardIsFlipped = cssCardIsFlipped;
+    } else if (selectedCard === "javascript") {
+        selectedCardIsFlipped = javascriptCardIsFlipped;
+    } else if (selectedCard === "design") {
+        selectedCardIsFlipped = designCardIsFlipped;
+    }
+
+    if (selectedCardIsFlipped === false) {
+        imageId.style.display = "none";
+        textId.style.display = "none";
+        listId.style.display= "block";
+    } 
+
+    if (selectedCardIsFlipped === true) {
+        imageId.style.display = "block";
+        textId.style.display = "block";
+        listId.style.display= "none";
+    }
+
+    if (selectedCard === "html") {
+        htmlCardIsFlipped = !htmlCardIsFlipped;
+    } else if (selectedCard === "css") {
+        cssCardIsFlipped = !cssCardIsFlipped;
+    } else if (selectedCard === "javascript") {
+        javascriptCardIsFlipped = !javascriptCardIsFlipped;
+    } else if (selectedCard === "design") {
+        designCardIsFlipped = !designCardIsFlipped;
+    }
 }
